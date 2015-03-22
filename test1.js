b4a063c795e09ee8a08a8c752673e1a8f83b9c85
Vince Jones <vincethebutcher@gmail.com>
2015-03-22 10:43:12 -0500

    Week 3

---
+++ test1.js
@@ -0,0 +1,5 @@
+function takeNum (num) {
+	console.log("I've take a variable into the function named \"num\" that is equal to "+num+" and is now being displayed")
+	num+=1;
+	console.log("I've now added 1 to the value and can display it here: "+num+" ");
+}
