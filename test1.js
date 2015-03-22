0e13806dd68781ccf90fab326655e4be2358802b
Vince Jones <vincethebutcher@gmail.com>
2015-03-22 13:50:55 -0500

    fix

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
-takeNum(6);
-
 function takeNum (num) {
 	console.log("I've take a variable into the function named \"num\" that is equal to "+num+" and is now being displayed")
 	num+=1;
 	console.log("I've now added 1 to the value and can display it here: "+num+" ");
 }
+
+takeNum(6);
